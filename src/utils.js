71cf26077ca104e0b82cce210dd311eefe7c02fc
bangnokia <bangnokia@gmail.com>
2025-05-27 14:50:57 +0700

    wip

---
+++ src/utils.js
@@ -0,0 +1,15 @@
+export async function loadGoogleFont(font, text) {
+	const url = `https://fonts.googleapis.com/css2?family=${font}&text=${encodeURIComponent(text)}`
+	console.log(url)
+	const css = await (await fetch(url)).text()
+	const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
+
+	if (resource) {
+		const response = await fetch(resource[1])
+		if (response.status == 200) {
+			return await response.arrayBuffer()
+		}
+	}
+
+	throw new Error('failed to load font data')
+}
